8fe025a3df820d6af8a98a78e43679eec9f58bb4
SebastianBruce <sebastianbruceschool@gmail.com>
2024-05-04 04:36:59 -0400

    many updates, now types each letter, error handling if it doesnt recognize the text

--- function/copyPasteText.py
+++ function/copyPasteText.py
@@ -1,8 +1,9 @@
 import pyperclip, pyautogui, keyboard
 
 def copyPaste(wordList):
-    # Copy the last word in the word list to the clipboard
-    pyperclip.copy(wordList[-1])
-    
-    # Simulate 'Ctrl + V' to paste the word
-    pyautogui.hotkey('ctrl', 'v')
+    if wordList != 0:
+        # Copy the last word in the word list to the clipboard
+        word = wordList[-1]
+        
+        # Simulate 'Ctrl + V' to paste the word
+        pyautogui.typewrite(word, interval=0.05)
